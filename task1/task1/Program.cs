9ed75d1ac4313b7e1015c009de2317702cf7a956
annaomelchenko1 <asomelch@mail.ru>
2025-01-27 22:07:08 +0300

    add task 1

---
+++ task1/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void PrintNumbers(int current, int N)
+    {
+        if (current > N)
+            return;
+        Console.WriteLine(current);
+        PrintNumbers(current + 1, N);
+    }
+    
+    static void Main()
+    {
+        int M = 5;
+        int N = 10; 
+        PrintNumbers(M, N);
+    }
+}
