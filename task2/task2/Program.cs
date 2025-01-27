ac797f8e63947f689ecfdc4d0800c38791c06902
annaomelchenko1 <asomelch@mail.ru>
2025-01-27 22:07:45 +0300

    add task 2

---
+++ task2/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static int Ackerman(int n, int m)
+    {
+        if (n == 0)
+            return m + 1;
+        else if (m == 0)
+            return Ackerman(n - 1, 1);
+        else
+            return Ackerman(n - 1, Ackerman(n, m - 1));
+    }
+    
+    static void Main()
+    {
+        int n = 3; 
+        int m = 5;
+        Console.WriteLine("A(" + n + ", " + m + ") = " + Ackerman(n, m));
+    }
+}
