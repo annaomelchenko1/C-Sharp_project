e584f11105ed24c144c8ca83b2703d3792840cd6
annaomelchenko1 <asomelch@mail.ru>
2025-01-27 22:07:59 +0300

    add task 3

---
+++ task3/task3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void PrintArray(int[] array, int index)
+    {
+        if (index < 0)
+            return;
+        Console.WriteLine(array[index]); 
+        PrintArray(array, index - 1);
+    }
+    
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        PrintArray(array, array.Length - 1);
+    }
+}
